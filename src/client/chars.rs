611e31e7b88ea9a546590aae2dbbbaaf6ce1e772
Mothebad <mothebad@gmail.com>
2022-03-14 10:12:58 -0400

    better board, finished user in, and added scraper (not remotely performant)

---
+++ src/client/chars.rs
@@ -0,0 +1,1 @@
+pub const CHARS: &'static [char; 26] = &['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
--- src/client/main.rs
+++ src/client/main.rs
@@ -1,9 +1,12 @@
 //todo
-//generate more accurate board from boggle distribution
-//finish chain of user in
-//implement computer player
-//
+//COMPELTE generate more accurate board from actual boggle distribution
+//COMPLETE finish chain of user in
+//KINDA COMPLETE implement computer "player" (more of a scraper) <-- John's current task
+//add timer?
+
 mod platform;
+mod chars;
+mod dice;
 
 use rand::seq::SliceRandom; // 0.7.2
 
@@ -23,26 +26,29 @@ struct Previous {
 fn main() {
     println!("Hello, world!");
     // preset stuff
-    let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let jsons = load_jsons();
 
-    let jsons = load_jsons(&chars);
-
-    let board = rand_board(&chars);
+    let board = rand_board();
 
     print_board(&board);
-    print!("{:?}\n", &board);
+    //print!("{:?}\n", &board);
+
+    let scrape: Vec<String> = scraper(&board, &jsons);
+    println!("{:?}", scrape);
 
     //game loop
     loop {
-        let u_in = input(&"Guess a word:\n");
-        println!("word: {}", check_word(&u_in, &jsons));
-        println!("sub: {}", check_sub(&u_in, &jsons));
-        println!("contains: {}", contains_word(&u_in, &board))
-
+        let u_in = input(&"Guess a word:\n").to_lowercase();
+        println!("word: {}", is_word(&u_in, &jsons));
+        println!("sub: {}", is_sub_word(&u_in, &jsons));
+        println!("contains: {}", board_contains(&u_in, &board));
+        if check_input(&u_in, &board, &jsons) {
+            println!("{} is a valid word!", &u_in)
+        }
     }
 }
 
-fn contains_word(word: &String, board: &[[char; 4]; 4]) -> bool {
+fn board_contains(word: &String, board: &[[char; 4]; 4]) -> bool {
     for x in 0..4 {
         for y in 0..4 {
             let letter = word[0 .. 1].to_string();
@@ -58,14 +64,10 @@ fn contains_word(word: &String, board: &[[char; 4]; 4]) -> bool {
     return false
 }
 
-// fn check_board(word: &String, board: &[[char; 4]; 4]) {
-//     let g = 0;
-// }
-
 fn worm(x: usize, y: usize, prog: String, fin: &String, board: &[[char; 4]; 4], p: &Vec<Previous>) -> bool {
     let mut prev = p.clone();
     prev.push(Previous {x: x as i32, y: y as i32});
-    println!("x: {} y: {} prog: {} fin: {} prev: {:?} ", &x, &y, &prog, &fin, &prev);
+    //println!("x: {} y: {} prog: {} fin: {} prev: {:?} ", &x, &y, &prog, &fin, &prev);
     if &prog == fin {
         return true;
     }
@@ -79,14 +81,14 @@ fn worm(x: usize, y: usize, prog: String, fin: &String, board: &[[char; 4]; 4], 
             if newx > -1 && newx < 4 && newy > -1 && newy < 4 {
                 let newxy = Previous {x : newx, y : newy};
                 if !prev.contains(&newxy) {
-                    println!("newx: {} newy: {}", newx, newy);
+                    //println!("newx: {} newy: {}", newx, newy);
                     let mut new_prog: String = prog.clone();
                     let throwaway: [char; 4] = board[newx as usize];
                     let throwfarther: char = throwaway[newy as usize];
                     new_prog.push(throwfarther);
-                    println!("prog: {} new_prog: {} fin: {} comp: {}", &prog, &new_prog, &fin, substr_compare(&new_prog, &fin));
+                    //println!("prog: {} new_prog: {} fin: {} comp: {}", &prog, &new_prog, &fin, substr_compare(&new_prog, &fin));
                     if substr_compare(&new_prog, &fin) {
-                        println!("new worm with ({}, {})", newx, newy);
+                        //println!("new worm with ({}, {})", newx, newy);
                         let res = worm(newx as usize, newy as usize, new_prog, fin, board, &prev);
                         if res {
                             return true;
@@ -99,11 +101,59 @@ fn worm(x: usize, y: usize, prog: String, fin: &String, board: &[[char; 4]; 4], 
     return false
 }
 
-fn load_jsons(chars: &[char; 26]) -> Vec<Vec<String>> {
+fn scraper(board: &[[char; 4]; 4], jsons: &Vec<Vec<String>>) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
+    for x in 0..4 {
+        for y in 0..4 {
+            let prev: Vec<Previous> = Vec::new();
+            let letter = board[x][y].to_string();
+            scraper_worm(x, y, letter, &board, &prev, &mut res, &jsons);
+        }
+    }
+    return res;
+}
+
+fn scraper_worm(x: usize, y: usize, prog: String, board: &[[char; 4]; 4], p: &Vec<Previous>, res: &mut Vec<String>, jsons: &Vec<Vec<String>>) {
+    let mut prev = p.clone();
+    prev.push(Previous {x: x as i32, y: y as i32});
+    //println!("x: {} y: {} prog: {} fin: {} prev: {:?} ", &x, &y, &prog, &fin, &prev);
+    for relx in 0 .. 3 {
+        for rely in 0 .. 3 {
+            //println!("relx: {} rely: {}", relx, rely);
+            //println!("{}(x) + {}(relx) - 1 = {}", x, relx, x as i32 + relx as i32 - 1);
+            let newx: i32 = (x as i32) + (relx as i32) - 1;
+            //println!("{}(y) + {}(rely) - 1 = {}", y, rely, y as i32 + rely as i32 - 1);
+            let newy: i32 = (y as i32) + (rely as i32) - 1;
+            if newx > -1 && newx < 4 && newy > -1 && newy < 4 {
+                let newxy = Previous {x : newx, y : newy};
+                if !prev.contains(&newxy) {
+                    //println!("newx: {} newy: {}", newx, newy);
+                    let mut new_prog: String = prog.clone();
+                    let throwaway: [char; 4] = board[newx as usize];
+                    let throwfarther: char = throwaway[newy as usize];
+                    new_prog.push(throwfarther);
+                    //println!("prog: {} new_prog: {} fin: {} comp: {}", &prog, &new_prog, &fin, substr_compare(&new_prog, &fin));
+                    if is_sub_word(&new_prog, &jsons) {
+                        if is_word(&new_prog, &jsons) {
+                            if !res.contains(&new_prog) {
+                                println!("Pushing: {}", &new_prog);
+                                res.push(new_prog.clone());
+                            }
+                        }
+                        //println!("new worm with ({}, {})", newx, newy);
+                        scraper_worm(newx as usize, newy as usize, new_prog, &board, &prev, res, &jsons);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn load_jsons() -> Vec<Vec<String>> {
     let mut out_array: Vec<Vec<String>>= Vec::new();
     let mut i: i32 = 0;
     loop {
-        let path_str = gen_path(i, chars);
+        let path_str = gen_path(i);
         let path = Path::new(&path_str);
         let dict = json_to_string(path);
         let in_array: Vec<String> = serde_json::from_str(&dict).unwrap();
@@ -117,6 +167,8 @@ fn load_jsons(chars: &[char; 26]) -> Vec<Vec<String>> {
 }
 
 //--- FUNCTIONALITY FUNCTIONS ---
+
+//USER INPUT FUNCTIONS
 fn input(prompt: &str) -> String {
     print!("{}", prompt);
     let mut user_in = String::new();
@@ -124,6 +176,38 @@ fn input(prompt: &str) -> String {
     return user_in[0 .. user_in.len() - 2].to_string();
 }
 
+fn check_input(inp: &String, board: &[[char; 4]; 4], jsons: &Vec<Vec<String>>) -> bool {
+    if input_is_str(inp) {
+        if is_word(inp, jsons) {
+            if board_contains(inp, board) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+fn input_is_str(inp: &String) -> bool {
+    //assumes input is cleaned from \n and \r by input function
+    let trimmed = inp.trim().chars();
+    for x in trimmed {
+        if !(chars::CHARS.contains(&x)) {
+            return false;
+        }
+    }
+    return true
+}
+
+fn is_word (word: &String, jsons: &Vec<Vec<String>>) -> bool {
+    //assumes input is a string composed of only the 26 lowercase letters
+    let res = correct_json(&word, &jsons).iter().position(|x| x.eq(word));
+    if res != None {
+        return true
+    }
+    return false;
+}
+
+//JSON FUNCTIONS
 fn json_to_string(p: &Path) -> String {
     // Open the file in read-only mode with buffer.
     let mut f = File::open(p).unwrap();
@@ -132,33 +216,24 @@ fn json_to_string(p: &Path) -> String {
     return buffer;
 }
 
-fn gen_path(i: i32, chars: &[char; 26]) -> String {
+fn gen_path(i: i32) -> String {
     let mut path_str: String = String::new();
     path_str.push_str(platform::JSON_PATH);
-    path_str.push(chars[i as usize]);
+    path_str.push(chars::CHARS[i as usize]);
     path_str.push_str("subDict.json");
-    println!("{:?}", &path_str);
+    //println!("{:?}", &path_str);
     return path_str;
 }
 
 fn correct_json<'a>(word: &String, json: &'a Vec<Vec<String>>) -> &'a Vec<String> {
-    let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let first: char = word.chars().nth(0).unwrap();
-    let ind: usize = chars.iter().position(|&x| x == first).unwrap();
+    let ind: usize = chars::CHARS.iter().position(|&x| x == first).unwrap();
     return &json[ind];
 }
 
 // fn word(x: i32, y: i32, json: Vec<Vec<String>>) -> String {
 //     return String::new()
 // }
-
-fn check_word (word: &String, json: &Vec<Vec<String>>) -> bool {
-    let res = correct_json(&word, &json).iter().position(|x| x.eq(word));
-    if res != None {
-        return true
-    }
-    return false;
-}
 
 fn substr_compare(sub: &String, word: &String) -> bool {
     //println!("x = {} || word = {}", &sub.to_string(), &word);
@@ -170,7 +245,7 @@ fn substr_compare(sub: &String, word: &String) -> bool {
     return false
 }
 
-fn check_sub (sub: &String,  json: &Vec<Vec<String>>) -> bool {
+fn is_sub_word (sub: &String,  json: &Vec<Vec<String>>) -> bool {
     //println!("{}, {}", &len, &word);
     let res = correct_json(&sub, &json).iter().position(|x| substr_compare(&sub, &x));
     if res != None {
@@ -180,12 +255,23 @@ fn check_sub (sub: &String,  json: &Vec<Vec<String>>) -> bool {
 }
 
 //--- BOARD FUNCTIONS ---
-fn rand_board(chars: &[char; 26]) -> [[char; 4]; 4] {
+fn rand_board() -> [[char; 4]; 4] {
     let mut b = [['a' as char; 4]; 4];
-
+    let mut i: usize = 0;
     for x in 0..4 {
         for y in 0..4 {
-            b[x][y] = *chars.choose(&mut rand::thread_rng()).unwrap();
+            b[x][y] = *dice::DICE[i].choose(&mut rand::thread_rng()).unwrap();
+            i += 1;
+        }
+    }
+    return b;
+}
+
+fn _old_rand_board() -> [[char; 4]; 4] {
+    let mut b = [['a' as char; 4]; 4];
+    for x in 0..4 {
+        for y in 0..4 {
+            b[x][y] = *chars::CHARS.choose(&mut rand::thread_rng()).unwrap();
         }
     }
     return b;
