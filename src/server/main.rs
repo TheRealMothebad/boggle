8369553ade2ac39f65529bb69f9f2ab26e92963e
sebwyn <gogleboywerser@gmail.com>
2022-03-15 14:20:50 -0400

    should have a working server implementation

--- src/server/main.rs
+++ src/server/main.rs
@@ -5,6 +5,7 @@ use std::io::{Write, stdin, stdout};
 fn main() -> std::io::Result<()> {
     let server = server::Server::new();
 
+    server.send("Hello World!");
     //listen for input and kill the server if we receive quit
     loop {
         let mut s = String::new();
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,30 +1,38 @@
 use boggle::shared::task::Task;
 
 use std::{
-    sync::mpsc::{Sender, Receiver, TryRecvError},
-    sync::{mpsc, atomic::AtomicBool},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::mpsc,
+    sync::mpsc::{Receiver, Sender, TryRecvError},
     thread,
-    thread::JoinHandle,
-    io::Read,
+    time::Duration,
 };
 
-//use stoppable_thread;
+use stoppable_thread::{SimpleAtomicBool, StoppableHandle};
+
+struct Connection {
+    handle: StoppableHandle<()>,
+    sender: Sender<String>,
+}
 
 pub struct Server {
     m_sender: Sender<String>,
-    handle: Option<JoinHandle<()>>,
+    handle: Option<StoppableHandle<()>>,
 }
 
 impl Server {
     pub fn new() -> Self {
         let (m_sender, m_receiver) = mpsc::channel();
-        
-        let instance = Self {m_sender, handle: None};
 
-        instance.handle = stoppable_thread::spawn(|stopped| {
+        let mut instance = Self {
+            m_sender,
+            handle: None,
+        };
+
+        instance.handle = Some(stoppable_thread::spawn(|stopped| {
             Self::listen_loop(stopped, m_receiver);
-        });
+        }));
 
         instance
     }
@@ -33,51 +41,79 @@ impl Server {
         self.m_sender.send(message.to_string());
     }
 
-    pub fn kill(&self) {
-        self.handle.unwrap().stop().join();
+    pub fn kill(self) {
+        self.handle.unwrap().stop().join().expect("Failed to join");
     }
 
-    fn listen_loop(stopped: AtomicBool, receiver: Receiver<String>){
-        
+    fn listen_loop(stopped: &SimpleAtomicBool, receiver: Receiver<String>) {
         let listener = TcpListener::bind("127.0.0.1:1337").expect("Couldn't Bind to Socket");
         listener.set_nonblocking(true).unwrap();
 
-        let mut connections: Vec<Task> = Vec::new();
+        let mut connections: Vec<Connection> = Vec::new();
 
         while !stopped.get() {
             match listener.accept() {
                 Ok((connection, ..)) => {
-                    connections.push(Task::new(move |receiver| {
-                        Self::handle_client(connection, receiver);
-                    }));
+                    let (tx, rx) = mpsc::channel();
+                    connections.push(Connection {
+                        handle: stoppable_thread::spawn(move |stopped| {
+                            Self::handle_client(stopped, connection, rx)
+                        }),
+                        sender: tx,
+                    });
                 }
                 Err(..) => continue,
             };
+
+            //pass all the messages we receive to the connections
+            match receiver.try_recv() {
+                Ok(message) => {
+                    for conn in &connections {
+                        conn.sender
+                            .send(message.clone())
+                            .expect("Failed to send message to connection");
+                    }
+                    ()
+                }
+                Err(TryRecvError::Disconnected) => break,
+                Err(TryRecvError::Empty) => {}
+            }
 
             thread::sleep(Duration::new(1, 0));
         }
 
         //consume connections and kill them
         for conn in connections.into_iter() {
-            conn.kill();
+            conn.handle
+                .stop()
+                .join()
+                .expect("Failed to join connection handler");
         }
     }
 
-    fn handle_client(mut connection: TcpStream, rx: Receiver<bool>) {
+    fn handle_client(
+        stopped: &SimpleAtomicBool,
+        mut connection: TcpStream,
+        m_receiver: Receiver<String>,
+    ) {
         //handle the client
-        loop {
-            match rx.try_recv() {
-                Ok(_) | Err(TryRecvError::Disconnected) => {
-                    break;
-                }
-                Err(TryRecvError::Empty) => {}
-            }
-
+        while !stopped.get() {
             let mut message: String = String::from("");
             connection
                 .read_to_string(&mut message)
                 .expect("Error receiving");
             print!("{}", message);
+
+            match m_receiver.try_recv() {
+                Ok(message) => {
+                    connection
+                        .write(message.as_bytes())
+                        .expect("Failed to send message!");
+                    return ();
+                }
+                Err(TryRecvError::Disconnected) => break,
+                Err(TryRecvError::Empty) => {}
+            }
 
             thread::sleep(Duration::from_millis(33));
         }
